e8df6ef79bd3ba8458a28850cff707391296d9f2
FaiyazKarimAnnoor <149487767+Faiyaz-Saad@users.noreply.github.com>
2025-08-29 20:01:15 -0700

    New Changes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -93,7 +93,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "org.example.project"
+            packageName = "Wireless Communication"
             packageVersion = "1.0.0"
             includeAllModules = true
         }

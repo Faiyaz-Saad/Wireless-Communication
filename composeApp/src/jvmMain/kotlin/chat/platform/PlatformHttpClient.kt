5592598d5d4ebfd53eac71c9f44096786f54aaa1
FaiyazKarimAnnoor <149487767+Faiyaz-Saad@users.noreply.github.com>
2025-09-21 13:53:10 -0700

    Delete composeApp/src/jvmMain/kotlin/chat directory

--- composeApp/src/jvmMain/kotlin/chat/platform/PlatformHttpClient.kt
+++
@@ -1,9 +0,0 @@
-package chat.platform
-
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.websocket.*
-
-actual fun platformHttpClient(): HttpClient = HttpClient(CIO) {
-    install(WebSockets)
-}
